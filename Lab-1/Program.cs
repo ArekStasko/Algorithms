1551b1d935d076f7a0ff84a191885d337f706dc5
Arkadiusz Staśko <Arkadiusz.Stasko@microsoft.wsei.edu.pl>
2022-03-02 14:49:34 +0100

    Add my MinIndexMethod solution

--- Lab-1/Program.cs
+++ Lab-1/Program.cs
@@ -6,7 +6,28 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int result = MinIndexValue(new int[] { 0, 534, 188, 237, 10, 576, 13, 1234, 1, 677 });
+            Console.WriteLine(result);
         }
+
+        public static bool IsTwoDigit(int num)
+        {
+            if (num > 9 && num < 99) return true;
+            return false;
+        }
+
+        public static int MinIndexValue(int[] nums)
+        {
+            int min = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (!IsTwoDigit(nums[min])) min = i;
+                if (nums[i] < nums[min] && IsTwoDigit(nums[i])) min = i;
+            }
+
+            if (IsTwoDigit(nums[min])) return min;
+            return -1;
+        }
+
     }
 }
