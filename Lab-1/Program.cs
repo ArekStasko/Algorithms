92a107cc73aa93e0f7ab2ea2ba95fc35bf44fb7e
ArekStasko <arek.stasko@gmail.com>
2022-03-08 20:48:45 +0100

    Add solutions to KNumberInArray and GetSum problems

--- Lab-1/Program.cs
+++ Lab-1/Program.cs
@@ -11,14 +11,12 @@ namespace Lab_1
 
             int getSumResult = GetSum(new int[] { 1, 234, 2345, 2 }, 3);
             Console.WriteLine(getSumResult);
+
+            int kNumberInArrayResult = KNumberInArray(new int[] {4, 3, 2, 4, 6, 8}, 2);
+            Console.WriteLine(kNumberInArrayResult);
         }
 
-        public static bool IsTwoDigit(int num)
-        {
-            if (num > 9 && num < 99) return true;
-            return false;
-        }
-
+        public static bool IsTwoDigit(int num) => num.ToString().Length == 2;
         public static int MinIndexValue(int[] nums)
         {
             if (nums.Length == 0) return -1;
@@ -35,8 +33,11 @@ namespace Lab_1
         }
 
 
+        public static bool IsKDigit(int num, int k) => num.ToString().Length == k;
         public static int GetSum(int[] nums, int k)
         {
+            if (nums.Length == 0) return 0;
+
             int sum = 0;
             foreach (var num in nums) sum += num;
             int average = sum / nums.Length;
@@ -44,10 +45,28 @@ namespace Lab_1
             sum = 0;
             foreach(int num in nums)
             {
-                if (num < average) sum += num;
+                if (num < average && IsKDigit(num, k)) sum += num;
             }
 
             return sum;
         }
+
+
+        public static int KNumberInArray(int[] nums, int k)
+        {
+            if (nums.Length == 0) return -1;
+
+            int count = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (count == k) return nums[i - 1];
+
+                if (nums[i - 1] < nums[i]) count++;
+                else count = 1;
+            }
+            return -1;
+        }
+
+
     }
 }
