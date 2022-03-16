e2db2596e4f29f3a60100a8c16c05f9f6b890b6a
Arkadiusz Staśko <Arkadiusz.Stasko@microsoft.wsei.edu.pl>
2022-03-16 14:42:42 +0100

    add GetTotalSum algo

--- Lab-3/Program.cs
+++ Lab-3/Program.cs
@@ -6,8 +6,22 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetRestCoins(-1));
-            Console.WriteLine(fibonnaci(4));
+            //Console.WriteLine(GetRestCoins(128));
+            //Console.WriteLine(fibonnaci(4));
+
+            Item[] items = new Item[]
+            {
+            new Item(25, 1),
+            new Item(35, 2),
+            new Item(15, 1),
+            new Item(30, 3),
+            new Item(50, 3),
+            new Item(25, 1)
+            };
+            
+
+            int sum = GetTotalSum(items);
+            Console.WriteLine(sum);
         }
 
 
@@ -56,6 +70,58 @@ namespace Lab_2
         public static long fibonnaci(int n)
         {
             return fibMem(n, new Dictionary<int, long>());
+        }
+
+        public static long fibMemo(int n)
+        {
+            int mem1 = 0;
+            int mem2 = 0;
+            int result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                mem1 = mem2;
+                mem2 = result;
+                result = mem1 + mem2;
+            }
+            return result;
+        }
+
+        
+        public static int GetTotalSum(Item[] items)
+        {
+            var itemsList = items.ToList();
+            int sum = 0;
+            int bagSize = 0;
+
+            while (bagSize < 4 && itemsList.Count != 0) { 
+                var bestValue = itemsList[0];
+
+                for(int j = 1; j < itemsList.Count; j++)
+                { 
+                    if (bestValue.Value < itemsList[j].Value && bagSize + itemsList[j].Weight <= 4)
+                    {
+                        bestValue = items[j];
+                    }
+                }
+                itemsList.Remove(bestValue);
+                bagSize += bestValue.Weight;
+                sum += bestValue.Value;
+            }
+
+            return sum;
+        }
+
+    }
+
+    public class Item
+    {
+        public int Value { get; set; }
+        public int Weight { get; set; }
+
+        public Item(int value, int weight)
+        {
+            Value = value;
+            Weight = weight;
         }
     }
 }
